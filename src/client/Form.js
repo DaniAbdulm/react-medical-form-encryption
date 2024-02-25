c282ecb4437e61aa62c4fb132d94db20d4b4b9de
DaniAbdulm <daniabdulm@gmail.com>
2024-02-25 18:42:20 -0500

    Added Navbar as a component

--- src/client/Form.js
+++ src/client/Form.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CryptoJS from "crypto-js";
+import NavBar from "./components/Navbar";
 
 export default function Form() {
     //initialize state object for form values
@@ -44,20 +45,7 @@ export default function Form() {
 
     return (
         <div className="container">
-            <nav className="navbar">
-                <div className="logo">CipherMD</div>
-                <div className="nav-dots">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
-                    <circle cx="12" cy="12" r="1"></circle>
-                    <circle cx="12" cy="5" r="1"></circle>
-                    <circle cx="12" cy="19" r="1"></circle>
-                    </svg>
-                </div>
-            </nav>
-
-            <div className="form-header">
-                <h1 className="section-title">1. Patient Information</h1>
-            </div>
+            <NavBar headerTitle={'1. Patient Information'} />
 
             <div className="form-row">
                 <div className="input-element">
---
+++ src/client/components/Navbar.js
@@ -0,0 +1,27 @@
+
+export default function NavBar({headerTitle}) {
+
+    //hanldes clicking on dots event
+    const handleDots = (e) => {
+        alert('You clicked on Dots');
+    }
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="logo">CipherMD</div>
+                <a className="nav-dots" onClick={handleDots}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
+                    <circle cx="12" cy="12" r="1"></circle>
+                    <circle cx="12" cy="5" r="1"></circle>
+                    <circle cx="12" cy="19" r="1"></circle>
+                    </svg>
+                </a>
+            </nav>
+
+            <div className="form-header">
+                <h1 className="section-title">{headerTitle}n</h1>
+            </div>
+        </>
+    )
+}
