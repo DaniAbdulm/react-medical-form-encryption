3c9bb3773323f08ec5199c29e9965671f8a907a6
DaniAbdulm <daniabdulm@gmail.com>
2024-07-05 17:31:36 -0400

    Added search functionality

--- src/client/Dashboard.js
+++ src/client/Dashboard.js
@@ -8,6 +8,7 @@ const secretKey = 'secret-key';
 const Dashboard = () => {
     const [patients, setPatients] = useState([]);
     const [selectedPatients, setSelectedPatients] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -44,12 +45,23 @@ const Dashboard = () => {
         }
     };
 
+    const filteredPatients = patients.filter(patient => {
+        return (
+            patient.firstName.toLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+            patient.lastName.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+            patient.emailAddress.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+            patient.phoneNumber.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+            patient.healthCard.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+        ); 
+    });
+ 
+
     return (
         <div className="container">
             <NavBar headerTitle={'Dashboard'} />
             <div className="header-container">
                 <h1 className="header-title">Patients</h1>
-                <input className="db-search-bar" placeholder="Search..." />
+                <input className="db-search-bar" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
             </div>
             <div className="patient-table-buttons">
                 <button className="pt-edit-btn" onClick={handleEditButtonClick}>Edit</button>
@@ -71,7 +83,7 @@ const Dashboard = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {patients.map((patient, index) => (
+                        {filteredPatients.map((patient, index) => (
                             <tr key={index}>
                                 <td>
                                     <input 
